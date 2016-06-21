80f99386c2e9bb5b8bc7bd954e8ca0dc8a3924f8
Jeremy M. Reed <reeje76@gmail.com>
2016-06-21 08:41:28 -0600

    Unit tests for LongUtils.intArrayToLong(...)

--- src/main/java/utilities/LongUtilsTest.java
+++ src/main/java/utilities/LongUtilsTest.java
@@ -2,6 +2,8 @@ package utilities;
 
 import static org.junit.Assert.*;
 
+import java.security.InvalidParameterException;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -60,6 +62,40 @@ public class LongUtilsTest
 	public void test_longToIntArray_NullInput_ThrowsException() {
 		try {
 			LongUtils.longToIntArray( null, false );
+			fail("Expected to catch NullPointerException");
+		} catch ( NullPointerException npe ) {
+			
+		} catch ( Exception e ) {
+			fail("Supposed to catch NullPointerException");
+		}
+	}
+	
+	@Test
+	public void test_intArrayToLong_ValidInput_ExpectedResult() {
+		int[] input = { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		long expected = 100000000000L;
+		long actual = LongUtils.intArrayToLong( input );
+		
+		assertEquals( expected, actual );
+	}
+	
+	@Test
+	public void test_intArrayToLong_EmptyInput_ThrowsException() {
+		try {
+			int[] input = { };
+			LongUtils.intArrayToLong( input );
+			fail("Expected to catch InvalidParameterException");
+		} catch ( InvalidParameterException ipe ) {
+			
+		} catch ( Exception e ) {
+			fail("Supposed to catch InvalidParameterException");
+		}
+	}
+	
+	@Test
+	public void test_intArrayToLong_NullInput_ThrowsException() {
+		try {
+			LongUtils.intArrayToLong( null );
 			fail("Expected to catch NullPointerException");
 		} catch ( NullPointerException npe ) {
 			
